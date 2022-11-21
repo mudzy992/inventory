12fad069ddd3379e0ca6cc0e063ec1b8a175bff9
Mudzahid Ceric <83641233+mudzy992@users.noreply.github.com>
2022-11-21 15:24:45 +0100

    Update database.configuration.ts

--- config/database.configuration.ts
+++ config/database.configuration.ts
@@ -1,8 +1,15 @@
-export const DatabaseConfiguration = {
+/* export const DatabaseConfiguration = {
   hostname: 'localhost',
   database: 'inventory',
   username: 'root',
   password: 'root',
+}; */
+
+export const DatabaseConfiguration = {
+  hostname: 'sql7.freemysqlhosting.net',
+  username: 'sql7579268',
+  password: '2pEUiVWZdF',
+  database: 'sql7579268',
 };
 
 // root - Nije,kikiriki1
