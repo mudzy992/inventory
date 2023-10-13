8b0fd12f346c4210f8f702c3bc09b4d008a86f37
Mudzahid Ceric <83641233+mudzy992@users.noreply.github.com>
2023-10-14 00:10:56 +0200

    Update deploy-frontend.sh

--- deploy-scripts/deploy-frontend.sh
+++ deploy-scripts/deploy-frontend.sh
@@ -10,7 +10,7 @@ if [[ "$1" != "$SECRET_KEY" ]]; then
 fi
 
 # Postavite putanju do vašeg repozitorija
-REPO_PATH="Documents/GitHub/frontend-inventory/"
+REPO_PATH="Documents/GitHub/frontend-inventory"
 
 # Navigirajte do repozitorija
 cd $REPO_PATH
