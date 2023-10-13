a381062420bd32e3a6e889e0a3e1063070726619
Mudzahid Ceric <83641233+mudzy992@users.noreply.github.com>
2023-10-14 00:12:23 +0200

    Update deploy-frontend.sh

--- deploy-scripts/deploy-frontend.sh
+++ deploy-scripts/deploy-frontend.sh
@@ -24,7 +24,7 @@ npm install
 
 # Ponovno pokrenite aplikaciju pomoću PM2 ili nekog drugog proces managera
 # Ako PM2 nije instaliran, možete zamijeniti s `node dist/src/main.js`
-pm2 start src
+pm2 start src/
 
 # Skripta je uspješno završena
 echo "Ažuriranje i ponovno pokretanje uspješno završeno za frontend."
