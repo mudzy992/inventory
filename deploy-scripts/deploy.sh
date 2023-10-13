c6bf955628099992aed9fe0c69bea862153aec89
Mudzahid Ceric <83641233+mudzy992@users.noreply.github.com>
2023-10-14 00:58:19 +0200

    Update deploy.sh

--- deploy-scripts/deploy.sh
+++ deploy-scripts/deploy.sh
@@ -18,14 +18,14 @@ cd $REPO_PATH
 # Provjerite status PM2 instance za vašu aplikaciju
 if pm2 info inventory-backend >/dev/null 2>&1; then
   # Ako PM2 instanca već postoji, izvršite restart
-  pm2 restart inventory-backend
+  pm2 restart inventory-backend --watch
 else
   # Ako PM2 instanca ne postoji, pokrenite novu
   # Instalirajte sve ovisnosti
   npm install
 
   # Pokrenite aplikaciju pomoću PM2
-  pm2 start dist/src/main.js --name inventory-backend
+  pm2 start dist/src/main.js --name inventory-backend --watch
 fi
 
 # Skripta je uspješno završena
