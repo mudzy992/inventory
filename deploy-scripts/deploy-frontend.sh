8af1a936bb6baefe573266ec3beca84a94ef0d51
Mudzahid Ceric <83641233+mudzy992@users.noreply.github.com>
2023-10-18 12:52:34 +0200

    Ecosystem

--- deploy-scripts/deploy-frontend.sh
+++ deploy-scripts/deploy-frontend.sh
@@ -9,28 +9,29 @@ if [[ "$1" != "$SECRET_KEY" ]]; then
   exit 1
 fi
 
-# Postavite putanju do vašeg repozitorija
+# Postavite putanju do vašeg repozitorija (i React projekta ako je ista putanja)
 REPO_PATH="/home/administrator/Documents/GitHub/frontend-inventory/"
 
 # Navigirajte do repozitorija
 cd $REPO_PATH
 
 # Resetirajte repozitorij na najnoviju verziju na produkcioni grani
-#PRODUCTION_BRANCH="AI-optimizacija"
-
 # Dobijte informacije o trenutnom branch-u iz webhook događaja
 BRANCH=$(echo $2 | cut -d/ -f3)
 
 git checkout $BRANCH
-#git checkout $PRODUCTION_BRANCH
 git pull
+
+# Navigirajte do direktorijuma sa React aplikacijom (ako je razdvojeno)
+# REACT_APP_PATH="/putanja/do/React/aplikacije"
+# cd $REACT_APP_PATH
 
 # Instalirajte sve ovisnosti pomoću npm
 npm install
 
 # Pokrenite ili restartajte aplikaciju koristeći PM2
 # Ovdje zamijenite 'your_app_name' s odgovarajućim imenom za vašu aplikaciju
-pm2 startOrRestart ecosystem.config.js --env production --update-env
+pm2 startOrRestart /home/administrator/Documents/GitHub/frontend-inventory/deploy-scripts/ecosystem.config.js --env production
 
 # Skripta je uspješno završena
 echo "Ažuriranje i ponovno pokretanje uspješno završeno za FrontEnd."
--- deploy-scripts/ecosystem1.config.js
+++ deploy-scripts/ecosystem1.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     apps : [{
       name: 'inventory-frontend', // Zamijenite s odgovarajućim imenom za vašu aplikaciju
-      script: '/home/administrator/Documents/GitHub/frontend-inventory/node_modules/react-scripts/bin/react-scripts.js',
+      script: 'npm',
       args: 'start',
       interpreter: 'none',
       env: {
