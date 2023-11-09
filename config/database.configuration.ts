4043cf94abdf54d42608f69903e9cc9b15405a30
Mudzahid Ceric <m.ceric@epbih.ba>
2023-11-09 17:47:14 +0100

    New databaseconfig

--- config/database.configuration.ts
+++ config/database.configuration.ts
@@ -3,31 +3,13 @@ export const DatabaseConfiguration = {
   database: 'inventory_v2',
   username: 'mudzy',
   password: 'nije,kikiriki',
-  // password: 'Mojalozinka!1',
   port: 3306,
-}; 
+}; //PRODUKCIONA
 
 /* export const DatabaseConfiguration = {
-  hostname: 'sql7.freemysqlhosting.net',
-  username: 'sql7579268',
-  password: '2pEUiVWZdF',
-  database: 'sql7579268',
-}; */
-
-// export const DatabaseConfiguration = {
-//   hostname: 'containers-us-west-38.railway.app',
-//   username: 'root',
-//   password: 'cMNEK4AOceWPqHEkyHcR',
-//   database: 'railway',
-//   port: 5598,
-// };
-
-/* export const DatabaseConfiguration = {
-   hostname: 'https://sql110.infinityfree.com',
-   username: 'if0_34984097',
-   password: 'uBfR76t2rd',
-   database: 'if0_34984097_inventory',
-   port: 3306,
- }; */
-
-// root - Nije,kikiriki1
+  hostname: '77.221.31.121',
+  database: 'inventory_test',
+  username: 'mudzy',
+  password: 'nije,kikiriki',
+  port: 3306,
+}; //TESTNA */
