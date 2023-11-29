450d1cdf3c05ed470c1a87ff9b45752df293b874
Mudzahid Ceric <m.ceric@epbih.ba>
2023-11-29 18:44:45 +0100

    Novi podaci na user

--- src/dtos/user/add.employe.article.dto.ts
+++ src/dtos/user/add.employe.article.dto.ts
@@ -1,9 +1,25 @@
+import { IsInt, IsNotEmpty, IsNumber, IsString } from 'class-validator';
+
 export class AddEmployeArticleDto {
+  @IsInt()
   articleId: number;
+
+  @IsInt()
   documentId: number;
+
+  @IsNumber()
+  @IsNotEmpty()
   value: number | null;
+
+  @IsString()
+  @IsNotEmpty()
   comment: string | null;
+
+  @IsString()
+  @IsNotEmpty()
   serialNumber: string;
+
+  @IsString()
+  @IsNotEmpty()
   invBroj: string;
-  status: 'zaduženo' | 'razduženo' | 'otpisano';
 }
