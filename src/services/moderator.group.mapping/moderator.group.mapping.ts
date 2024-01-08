0e3eced090693999d39bc98ce5a122e42fae0295
Mudzahid Ceric <83641233+mudzy992@users.noreply.github.com>
2024-01-08 15:34:05 +0100

    Implementirana selekcija grupe

--- src/services/moderator.group.mapping/moderator.group.mapping.ts
+++ src/services/moderator.group.mapping/moderator.group.mapping.ts
@@ -3,6 +3,7 @@ import { InjectRepository } from "@nestjs/typeorm";
 import { TypeOrmCrudService } from "@nestjsx/crud-typeorm";
 import { ModeratorGroupMappingDTO } from "src/dtos/moderator.group.mapping/moderator.group.mapping.dto";
 import { ModeratorGroupMapping } from "src/entities/ModeratorGroupMapping";
+import { TicketGroup } from "src/entities/TicketGroup";
 import { ApiResponse } from "src/misc/api.response.class";
 import { Repository } from "typeorm";
 
@@ -11,34 +12,38 @@ export class ModeratorGroupMappingService extends TypeOrmCrudService<ModeratorGr
   constructor(
     @InjectRepository(ModeratorGroupMapping)
     private readonly moderatorGroup: Repository<ModeratorGroupMapping>,
+    @InjectRepository(TicketGroup)
+    private readonly ticketGroup: Repository<TicketGroup>,
   ) {
     super(moderatorGroup);
   }
 
   async getAllGroupModerators(): Promise<ModeratorGroupMappingDTO[] | ApiResponse> {
     const moderators = await this.moderatorGroup.find({
-        relations: [
-            "user",
-            "group",
-            "group.parentGroup",
-            "group.ticketGroups",
-            "group.location"
-        ]
+      relations: ["user", "group", "group.location"],
     });
-
-    const response: ModeratorGroupMappingDTO[] = await moderators.map((item) => ({
-      group: {
-        groupId: item.group.groupId,
-        groupName: item.group.groupName,
-        location: item.group.location,
-      },
-      user: {
-        userId: item.user.userId,
-        fullname: item.user.fullname,
-      }
-    }))
   
-    if (!response) {
+    const response: ModeratorGroupMappingDTO[] = await Promise.all(moderators.map(async (item) => {
+      const subGroups = await this.ticketGroup.find({where: {parentGroupId: item.groupId}})
+      return {
+        groupId: item.groupId,
+        group: {
+          groupId: item.group.groupId,
+          groupName: item.group.groupName,
+          location: item.group.location,
+          ticketGroups: subGroups ? subGroups.map((sub) => ({
+            groupId: sub.groupId,
+            groupName: sub.groupName,
+          })) : [],
+        },
+        user: {
+          userId: item.user.userId,
+          fullname: item.user.fullname,
+        },
+      };
+    }));
+  
+    if (!moderators) {
       return new ApiResponse('error', -11002, 'Ticket not found.');
     }
     return response;
