7cddfc32ed1f658433139ed5c7aee3272a10eb2c
Mudzahid Ceric <83641233+mudzy992@users.noreply.github.com>
2024-08-02 14:48:48 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,29 @@ import { NestFactory } from "@nestjs/core";
 import { NestExpressApplication } from "@nestjs/platform-express";
 import { StorageConfig } from "config/storage.config";
 import { AppModule } from "./app.module";
+import * as cors from 'cors';
+import { ValidationPipe } from "@nestjs/common";
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const allowedOrigins = [
+    'http://localhost:4001',
+    'http://inventory.hopto.org:4001',
+  ];
+
+  const corsOptions = {
+    origin: function (origin, callback) {
+      if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  };
+  app.use(cors(corsOptions));
+  app.useGlobalPipes(new ValidationPipe());
   app.useStaticAssets(StorageConfig.prenosnica.destination, {
     prefix: StorageConfig.prenosnica.urlPrefix,
     maxAge: StorageConfig.prenosnica.maxAge,
