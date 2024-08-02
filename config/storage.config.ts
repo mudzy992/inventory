2784a07e680dc86fb122d69e16d48d440a0ebf71
Mudzahid Ceric <83641233+mudzy992@users.noreply.github.com>
2024-08-03 01:02:16 +0200

    Update storage.config.ts

--- config/storage.config.ts
+++ config/storage.config.ts
@@ -1,6 +1,6 @@
 export const StorageConfig = {
   prenosnica: {
-    template: "/usr/src/app/storage/prenosnica.docx",
+    template: "/usr/src/app/storage/prenosnica/template/prenosnica.docx",
     destination: "/usr/src/app/storage/prenosnica/", 
     urlPrefix: "/prenosnica/",
     maxAge: 1000 * 60 * 60 * 24 * 7,
